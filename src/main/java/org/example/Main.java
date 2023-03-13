ab60b8b43a85c81bbcbd798d5e4bd8e7097c78b1
Flylink <flylink92@mail.ru>
2023-03-14 00:50:03 +0700

    Разделены все функции согласно SOLID

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,26 @@
 package org.example;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import org.example.customer.*;
+import org.example.products.*;
+import org.example.sales.*;
+
 
 public class Main {
-
     public static void main(String[] args) {
-        HashMap<String, Integer> products = new HashMap<>();
-        products.put("Хлеб", 56);
-        products.put("Масло", 153);
-        products.put("Колбаса", 211);
-        products.put("Пирожок", 45);
 
-        System.out.println("В МАГАЗИНЕ В НАЛИЧИИ");
-        for (Map.Entry<String, Integer> productAndPrice : products.entrySet()) {
-            System.out.println(productAndPrice.getKey() + " за " + productAndPrice.getValue() + " руб./шт.");
-        }
-
-        System.out.println("Введите два слова: название товара и количество. Или end");
-        Scanner scanner = new Scanner(System.in);
-        Purchase purchase = new Purchase();
-        while (true) {
-            String line = scanner.nextLine();
-            if ("end".equals(line)) {
-                break;
-            }
-            String[] parts = line.split(" ");
-            String product = parts[0];
-            int count = Integer.parseInt(parts[1]);
-            purchase.addPurchase(product, count);
-        }
-        long sum = purchase.sum(products);
-        System.out.println("ИТОГО: " + sum);
+        Product apple = new Product("Apple");
+        Product peach = new Product("Peach");
+        Product pear = new Product("Pear");
+        apple.setRetailPrice(20);
+        peach.setRetailPrice(30);
+        pear.setRetailPrice(25);
+        StorageService mainStorage = new StorageServiceImpl("Основное хранилище");
+        mainStorage.addItem(apple, 10);
+        mainStorage.addItem(pear, 5);
+        mainStorage.addItem(peach, 5);
+        mainStorage.getStorageContents();
+        SalesRegistrationService salesRegistration = new SalesRegistrationServiceImpl(mainStorage);
+        CustomerService customerService = new CustomerServiceImpl(mainStorage, salesRegistration);
+        customerService.start();
     }
 }
